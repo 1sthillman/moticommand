a16545870e62f3f5a8da789caf901b947d276a90
Example User <example@example.com>
2025-12-30 02:36:50 +0300

    Fix: Config basePath conditional for Mobile vs GitHub Pages

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.moticommand.app',
+  appName: 'Moticommand',
+  webDir: 'out'
+};
+
+export default config;
